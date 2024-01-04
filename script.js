7fd513adfc273c15e3331bc1ed616c5898105da2
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2024-01-03 20:12:43 -0600

    Finsih board styling and start drawing board and snake

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("pickachu");
+const board = document.getElementById("game-board");
+
+let snake = [{ x: 10, y: 10 }];
+
+//Draw game map, snake and food
+function draw() {
+  board.innerHTML = "";
+  drawSnake();
+}
+
+function drawSnake() {}
