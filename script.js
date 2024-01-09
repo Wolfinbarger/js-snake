5934f108406a6e63870b907124773c827e198ade
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2024-01-08 20:20:59 -0600

    draw snake and food on board

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 "use strict";
 
 const board = document.getElementById("game-board");
+const gridSize = 20;
 
 let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
 
 //Draw game map, snake and food
 function draw() {
   board.innerHTML = "";
   drawSnake();
+  drawFood();
 }
 
-function drawSnake() {}
+function drawSnake() {
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement("div", "snake");
+
+    setPosition(snakeElement, segment);
+    board.appendChild(snakeElement);
+  });
+}
+
+// Create a snake or food cube
+function createGameElement(tag, className) {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
+
+// Set position of snake or food
+function setPosition(element, position) {
+  element.style.gridColumn = position.x;
+  element.style.gridRow = position.y;
+}
+
+// Testing draw function
+draw();
+
+function drawFood() {
+  const foodElement = createGameElement("div", "food");
+  setPosition(foodElement, food);
+  board.appendChild(foodElement);
+}
+
+function generateFood() {
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
+}
